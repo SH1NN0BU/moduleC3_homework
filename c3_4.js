4bbaaf083920b2f55c7835b9760198057e374bd6
SH1NN0BU <github@nesteryuk.moscow>
2023-02-10 21:17:18 +0300

    ElectricalAppliance

---
+++ c3_4.js
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////
+// Homework C3.4
+// Open c3_4.html to test results in console log
+//////////////////////////////////////////////////////
+
+function ElectricalAppliance(name, power) {
+    this.name = name;
+    this.power = power;
+    this.isPlugged = false;
+  }
