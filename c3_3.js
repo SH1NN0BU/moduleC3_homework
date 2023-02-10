d9af4e9cc6fa588f56bca4a2521d10cf0b2873f5
SH1NN0BU <github@nesteryuk.moscow>
2023-02-10 17:18:03 +0300

    checkPropInObj end createObjWithoutProto

--- c3_3.js
+++ c3_3.js
@@ -25,3 +25,17 @@ function printObject (obj) {
   // Написать функцию, которая принимает в качестве аргументов строку и объект, 
   // а затем проверяет, есть ли у переданного объекта свойство с данным именем. 
   // Функция должна возвращать true или false.
+
+  function checkPropInObj (str, obj) {
+    return (str in obj);
+  }
+  
+  console.log(checkPropInObj('name', junior))
+
+// Написать функцию, которая создает пустой объект, но без прототипа.
+
+function createObjWithoutProto() {
+    return Object.create(null);
+  }
+  
+  console.log(createObjWithoutProto())
