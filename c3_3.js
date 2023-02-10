da07380c023bb07772817fb12b0af4b1f0f4eae3
SH1NN0BU <github@nesteryuk.moscow>
2023-02-10 17:09:42 +0300

    printObject

--- c3_3.js
+++ c3_3.js
@@ -2,3 +2,26 @@
 // Homework C3.3
 // Open c3_3.html to test results in console log
 //////////////////////////////////////////////////////
+
+const junior = {
+    city : "Moscow",
+    name : "Alexander"
+  }
+
+// Написать, функцию, которая принимает в качестве аргумента объект и выводит в консоль все ключи и значения только собственных свойств. 
+// Данная функция не должна возвращать значение.
+
+function printObject (obj) {
+    console.log(`Printing fields of ${obj}`);
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        console.log(prop, obj[prop]);
+      }
+    }
+  }
+
+  printObject(junior)
+
+  // Написать функцию, которая принимает в качестве аргументов строку и объект, 
+  // а затем проверяет, есть ли у переданного объекта свойство с данным именем. 
+  // Функция должна возвращать true или false.
